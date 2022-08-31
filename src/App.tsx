f69c92fa32088e5284bcddb5b1b41bd069c4305d
jennyjunho2 <a01032208149@gmail.com>
2022-08-31 21:55:58 +0900

    220831

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { useRecoilState } from 'recoil';
-import './App.css';
+import './App.scss';
 import { isVideoLoadingAtom } from './atoms';
-import Noise from './Components/Noise';
+import Noise from './Components/Noise/Noise';
 
 function App() {
   const [isVideoLoading, setIsVideoLoading] = useRecoilState<boolean>(isVideoLoadingAtom);
 
   return (
     <div className="App">
-      {<Noise />}
+      {isVideoLoading && <Noise />}
+      <div className="video-background">
+
+      </div>
+      <div className="Sidebar">
+        <div className="TitleContainer">
+          <span>Lorem ipsum</span>
+        </div>
+        <div className="StreetSoundContainer">
+
+        </div>
+      </div>
     </div>
   );
 }
