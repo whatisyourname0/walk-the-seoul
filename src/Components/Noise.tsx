8d95b44cd19162855cf0fc550ef534fcdff54cb9
jennyjunho2 <a01032208149@gmail.com>
2022-08-31 17:18:00 +0900

    initial Commit

---
+++ src/Components/Noise.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { useRecoilState } from "recoil";
+import { isVideoLoadingAtom } from "../atoms";
+import "./_Noise.scss";
+
+function Noise() {
+  const [videoLoading, setVideoLoading] = useRecoilState<boolean>(isVideoLoadingAtom);
+
+  return (
+    videoLoading ? <div className="Noise"></div> : null
+  );
+}
+
+export default Noise;
